df0a3e632fd357e759adc2000014354d5a4203cf
Ryan Moore <moorer@udel.edu>
2018-08-24 14:57:34 -0400

    Add some tests

--- lib/matr.rb
+++ lib/matr.rb
@@ -1,5 +1,96 @@
 require "matr/version"
 
 module Matr
-  # Your code goes here...
+  def self.default_opt opts, key, default
+    opts.has_key?(key) ? opts[key] : default
+  end
+
+  def self.main opts
+
+    infile             = opts[:infile]
+    self_score         = self.default_opt opts, :self_score, 100
+    missing_score      = self.default_opt opts, :missing_score, 0
+    output_style       = self.default_opt opts, :output_style, "wide"
+    ensure_symmetry    = true # self.default_opt opts, :ensure_symmetry, true
+    ensure_self_scores = self.default_opt opts, :ensure_self_scores, true
+
+    unless File.exist? infile
+      raise StandardError, "FATAL -- infile '#{infile}' does not exist"
+    end
+
+    unless output_style == "wide" || output_style == "long"
+      raise StandardError, "FATAL -- output style must be either wide or long"
+    end
+
+    graph    = Hash.new { |ht, k| ht[k] = Hash.new }
+    all_keys = Set.new
+    header   = nil
+
+    File.open(infile, "rt").each_line.with_index do |line, idx|
+      line.chomp!
+
+      if idx.zero?
+        header = line
+      else
+        source, target, score = line.split "\t"
+
+        all_keys << source << target
+
+        # Fix self score if the option is there.
+        if source == target && ensure_self_scores
+          score = self_score
+        end
+
+        # If you have duplicates with different scores, that's an error: a
+        # => b = 10 and a => b = 20
+        if graph[source].has_key?(target) && graph[source][target] != score
+          raise StandardError, "FATAL -- source--target (#{source}--#{target}) was repeated with a different score"
+        end
+
+        graph[source][target] = score
+
+        if ensure_symmetry
+          # Since you might have a files that specifies a => b = 10, and b
+          # => a = 10, that would be okay.  But if you have a => b = 10, and
+          # b => a = 5, then that would be an error.
+          if graph[target].has_key?(source) && graph[target][source] != score
+            raise StandardError, "FATAL -- target--source (#{target}--#{source}) is specified seperately from source--target (#{source}--#{target}), BUT their scores do not match.  This means your matrix is NOT symmetric.  Did you really mean to pass the --ensure-symmetry flag?"
+          end
+
+          graph[target][source] = score
+        end
+
+        # Makes sure these weren't already specified earlier.
+        unless graph[target].has_key? target
+          graph[target][target] = self_score
+        end
+
+        unless graph[source].has_key? source
+          graph[source][source] = self_score
+        end
+      end
+    end
+
+    if output_style == "long"
+      puts header
+
+      all_keys.each do |source|
+        all_keys.each do |target|
+          puts [source, target, graph[source][target] || missing_score].join "\t"
+        end
+      end
+    else
+      all_keys = all_keys.sort
+
+      puts ["", all_keys].join "\t"
+
+      all_keys.each do |source|
+        row = all_keys.map do |target|
+          graph[source][target] || missing_score
+        end
+
+        puts [source, row].join "\t"
+      end
+    end
+  end
 end
--- spec/matr_spec.rb
+++ spec/matr_spec.rb
@@ -1,9 +1,116 @@
+TEST_FILE_DIR = File.join __dir__, "test_files"
+FATAL_ERROR   = StandardError
+
+def infile fname
+  File.join TEST_FILE_DIR, fname
+end
+
 RSpec.describe Matr do
   it "has a version number" do
     expect(Matr::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+
+  describe "::main" do
+    context "things that raise errors" do
+      it "raises error with mismatched scores" do
+        fname = "score_mismatch.txt"
+
+        expect { Matr.main infile: infile(fname) }.to raise_error FATAL_ERROR
+      end
+
+      context "--ensure-symmetry" do
+        it "raises error with non-symmetric input" do
+          fname = "not_symmetric.txt"
+          opts = {
+            infile: infile(fname),
+            ensure_symmetry: true
+          }
+
+          expect { Matr.main opts }.to raise_error FATAL_ERROR
+        end
+
+        it "adds in self hits and reverse hits symmetrically" do
+          fname = "adds_self_and_rev_hits.txt"
+          expected = File.read(infile(fname) + ".expected")
+
+          opts = {
+            infile: infile(fname),
+            ensure_symmetry: true,
+            self_score: 100
+          }
+
+          expect { Matr.main opts }.to output(expected).to_stdout
+        end
+      end
+
+=begin
+      context "--no-ensure-symmetry" do
+        it "doesn't raise error with non-symmetric input" do
+          fname = "not_symmetric.txt"
+          expected = File.read(infile(fname) + ".expected")
+
+          opts = {
+            infile: infile(fname),
+            ensure_symmetry: false
+          }
+
+          expect { Matr.main opts }.to output(expected).to_stdout
+        end
+
+        it "adds in self hits and reverse hits (defualt self hit value)" do
+          fname = "adds_self_and_rev_hits.txt"
+          expected = File.read(infile(fname) + ".no_sym.expected")
+
+          opts = {
+            infile: infile(fname),
+            ensure_symmetry: false,
+            self_score: 100,
+            missing_score: 0
+          }
+
+          expect { Matr.main opts }.to output(expected).to_stdout
+        end
+      end
+    end
+=end
+
+    end
+
+    context "--ensure-self-scores" do
+      it "replaces self scores with specified value" do
+        fname = "wrong_self_score.txt"
+        expected = File.read(infile(fname) + ".expected")
+
+        opts = {
+          infile:             infile(fname),
+          self_scores:        100,
+          ensure_self_scores: true
+        }
+
+        expect { Matr.main opts }.to output(expected).to_stdout
+      end
+    end
+
+    context "--no-ensure-self-scores" do
+      it "leaves self scores that don't match" do
+        fname = "wrong_self_score.txt"
+        expected = File.read(infile(fname) + ".no_fix.expected")
+
+        opts = {
+          infile:             infile(fname),
+          self_scores:        100,
+          ensure_self_scores: false
+        }
+
+        expect { Matr.main opts }.to output(expected).to_stdout
+      end
+    end
+
+    it "checks for duplicates with case sensitivity" do
+      fname = "case_sensitive.txt"
+
+      expect { Matr.main infile: infile(fname) }.not_to raise_error
+    end
   end
 end
